078ca464dee22bdcad7e4386b182d550f9cc3617
KarimulHasan <karimul172@gmail.com>
2026-02-26 19:04:40 +0600

    add item to interview array function added

--- js/script.js
+++ js/script.js
@@ -31,4 +31,32 @@ function toggleStyle(id){
 }
 
 const mainContainer = document.querySelector('main');
-console.log(mainContainer);
+mainContainer.addEventListener('click', function(event){
+    // console.log(event.target.parentNode.parentNode.parentNode);
+    const parentNode = event.target.parentNode.parentNode.parentNode;
+    // console.log(event.target.value);
+    if(event.target.innerText === 'INTERVIEW') {
+        parentNode.classList.add('border-l-3', 'border-green-400');
+    }
+    else if (event.target.innerText === 'REJECTED'){
+        parentNode.classList.add('border-l-3', 'border-red-400');
+    }
+    
+    const companyName = parentNode.querySelector('.company-name').innerText;
+    const jobName = parentNode.querySelector('.job-name').innerText;
+    const salary = parentNode.querySelector('.salary').innerText;
+    const status = parentNode.querySelector('.status').innerText;
+    const notes = parentNode.querySelector('.notes').innerText;
+    // console.log(companyName, jobName, salary, status, notes);
+    const cardInfo = {
+        companyName,
+        jobName,
+        salary,
+        status,
+        notes
+    }
+    const interviewExist = interviewList.find(item => item.companyName === cardInfo.companyName);
+    if(!interviewExist) {
+        interviewList.push(cardInfo);
+    }
+})
